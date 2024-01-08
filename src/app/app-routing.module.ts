cde8189ce32d1e1d798c82bd4eea1012686039b8
hendrang <hendrang@gmail.com>
2024-01-07 20:20:59 -0700

    Initial Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ResumeComponent } from './components/resume/resume.component';
+
+const routes: Routes = [
+  {
+    path: "home",
+    component: HomeComponent,
+  },
+  {
+    path: "resume",
+    component: ResumeComponent,
+  },
+  {
+    path: "",
+    redirectTo: "/home",
+    pathMatch: "full",
+  },
+  { path: "**", redirectTo: "/home"}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
